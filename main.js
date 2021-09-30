be1135212d29e0e500f8d47e656ac22613824e1f
Jaruwat Pohong <jara7897@gmail.com>
2021-09-30 14:02:55 +0700

    feat: add project files

---
+++ main.js
@@ -0,0 +1,136 @@
+#!/usr/bin/env node
+
+console.log('Started Dyz-Obfuscator!')
+
+import program from 'commander'
+import path from 'path'
+import fse from 'fs-extra'
+import readline from 'readline';
+
+
+import {
+	getExtension,
+	directoryExists,
+	readdir,
+	mkdir,
+	rm,
+	obfuscateHTML,
+	obfuscateCSS,
+	obfuscateJS,
+	sleep
+} from './lib/index.js'
+
+program
+	.version('1.0.0')
+	.name('dobs')
+	.description('An image resizer to make thumbnails')
+	.option('-s,--source [folder]', 'Source images directory', './')
+	.option(
+		'-o,--outout [folder]',
+		'Directory to be created for obfuscated files',
+		'./dyz-obfuscated'
+	)
+	.parse(process.argv)
+
+const WhiteListExtension = ['html', 'css', 'js'];
+
+const main = async () => {
+	try {
+		// Use current working dir vs __dirname where this code lives
+		const cwd = process.cwd()
+
+		// Use user input or default options
+		const {
+			source,
+			outout
+		} = program.opts()
+
+
+		const srcPath = source.replace(/\\/g, '/')
+		const destPath = outout.replace(/\\/g, '/')
+		//console.log(source, outout)
+
+		// Remove destination directory is it exists
+		if (directoryExists(destPath)) {
+			await rm(destPath)
+		}
+
+		// Create destination directory
+		await mkdir(destPath)
+
+
+		// Read source directory
+		const filesAll = await readdir(srcPath)
+
+		// Create task list
+		let taskList = []
+		//filesAll.forEach(pathname => {
+		for (let index = 0; index < filesAll.length; index++) {
+			let pathname = filesAll[index];
+			let copypath = pathname.replace(srcPath, ""),
+				desPath = destPath + '/' + copypath;
+
+			process.stdout.write(`[${index+1}/${filesAll.length}] Copying file to ${desPath}`);
+			await sleep(100)
+			
+			if(WhiteListExtension.includes(getExtension(pathname))) taskList.push(pathname);
+			//console.log(desPath)
+
+			fse.copySync(pathname, desPath)
+			//console.log('Copied to ' + desPath)
+			readline.clearLine(process.stdout);
+			readline.cursorTo(process.stdout, 0);
+		};
+		//console.log(taskList, `Found ${taskList.length} items.`, srcPath)
+		console.log("[!] Finish copied files!")
+
+		console.log("\nStarting obfuscate task...")
+
+		for (let index = 0; index < taskList.length; index++) {
+			let pathname = taskList[index];
+			//console.log(stringSource)
+			let copypath = pathname.replace(srcPath, ""),
+				desPath = destPath + '/' + copypath;
+			//console.log(desPath)
+			
+			//console.log(pathname)
+			let stringSource = fse.readFileSync(pathname, 'utf8');
+
+			let obfuscatedOutput = "";
+			process.stdout.write(`[${index+1}/${taskList.length}] Obfuscating to ${desPath}`);
+			await sleep(100)
+			// Wipe line for next status
+
+			switch (getExtension(pathname).toLowerCase()) {
+				case "html":
+					obfuscatedOutput = obfuscateHTML(stringSource)
+					break;
+
+				case "css":
+					obfuscatedOutput = obfuscateCSS(stringSource)
+					break;
+
+				case "js":
+					obfuscatedOutput = obfuscateJS(stringSource)
+					break;
+
+				default:
+					break;
+			}
+			//fse.removeSync(desPath)
+			fse.writeFileSync(desPath, obfuscatedOutput, { flag: 'w' })
+			readline.clearLine(process.stdout);
+			readline.cursorTo(process.stdout, 0);
+			// fse.copySync(pathname, desPath)
+			// console.log('Copied to ' + desPath)
+		};
+
+		console.log('[!] Obfuscated files successfully!\n')
+
+		console.log(`All Files has been created at ${path.join(cwd, destPath)}`)
+	} catch (error) {
+		console.log('Error creating obfuscate file.', error)
+	}
+}
+
+main()
